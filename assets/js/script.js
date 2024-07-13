f09cb2b47f63e631708f3e1932ca114b8c0ecbf1
mohammedanafmaster <mohammedanafm@gmail.com>
2024-07-13 14:15:32 +0530

    Header added

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,28 @@ const preloader = document.querySelector("[data-preloader]");
 window.onload = () => {
     preloader.classList.add("remove");
 }
+
+// Add event on multiple elements
+const addEventOnElements = (elements, eventType, callback) => {
+    for (let i = 0 ; i < elements.length ; i++) {
+        elements[i].addEventListener(eventType, callback);
+    }
+}
+
+// Header
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navbar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+const header = document.querySelector("[data-header]");
+
+const navToggle = () => {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    docuument.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", navToggle);
+
+window.onscroll = () => {
+    header.classList[window.scrollY > 100 ? "add" : "remove"]("active");
+}
